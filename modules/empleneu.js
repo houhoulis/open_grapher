5794b2ba1d1d17d41501fa50ad5e3544d156e451
Chris Houhoulis <chris@chrishouhoulis.com>
2021-07-08 21:06:51 -0400

    Clean up language JS. Remove intro text from html.

--- modules/empleneu.js
+++ modules/empleneu.js
@@ -1,13 +1,16 @@
 export { empleneu, setAng };
-import { contentsFor } from './lang-selector.js';
+import { contentsFor, CATALÀ, ENGLISH } from './lang-selector.js';
 
-let exposició = 'català';
+let exposició = CATALÀ;
+const ésCatalà = (() => exposició === CATALÀ);
 
 function setAng(llengua) {
   exposició = llengua;
 };
 
-const ésCatalà = (() => exposició === 'català');
+function toggleAng() {
+  ésCatalà() ? setAng(ENGLISH) : setAng(CATALÀ);
+};
 
 function empleneu(llengua) {
   if (document.readyState === 'loading') {
@@ -21,21 +24,23 @@ function empleneu(llengua) {
 function empleneuDOMElements() {
   const content = contentsFor(exposició);
 
-  const llenguaText = document.querySelector('#llengua-canviar');
-  llenguaText.innerHTML = content['llenguaText'];
-  const bandera = document.querySelector('#bandera');
-  bandera.setAttribute('src', content['bandera']);
-  const títol = document.querySelector('#títol');
-  títol.innerHTML = content['títol'];
-  const intro = document.querySelector('#intro');
-  intro.innerHTML = content['intro'];
+  document.querySelector('#llengua-canviar').innerHTML = content['llenguaText'];
+  document.querySelector('#bandera').setAttribute('src', content['bandera']);
+  document.querySelector('#títol').innerHTML = content['títol'];
+  document.querySelector('#intro1').innerHTML = content['intro1'];
+  document.querySelector('#intro2').innerHTML = content['intro2'];
+  document.querySelector('#intro3').innerHTML = content['intro3'];
+  document.querySelector('#intro4').innerHTML = content['intro4'];
+  document.querySelector('#intro5').innerHTML = content['intro5'];
+  document.querySelector('#intro6').innerHTML = content['intro6'];
 
   updateLlenguatgeTag();
+  toggleAng();
 };
 
 function updateLlenguatgeTag() {
   const llenguatgeTag = document.querySelector('body');
-  if(ésCatalà) {
+  if(ésCatalà()) {
     llenguatgeTag.classList.add('català');
   } else {
     llenguatgeTag.classList.remove('català');
